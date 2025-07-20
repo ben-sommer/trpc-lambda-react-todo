536aa454b114184f925df9888f64a6f63edbe6b8
Ben Sommer <32485726+ben-sommer@users.noreply.github.com>
2025-07-20 18:59:09 +0100

    fix deployments

--- sst.config.ts
+++ sst.config.ts
@@ -37,7 +37,7 @@ export default $config({
             },
             build: {
                 command: "npm run build:client",
-                output: "dist",
+                output: "client/dist",
             },
         });
 
